3b84ede71bbed717033ea97dc92b964ab1a86a9a
ValentinG <valentin.gardie@viacesi.fr>
2020-05-08 01:51:30 +0200

    Creation de la partie login admin

---
+++ ADO/Administrator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ADO
+{
+    public class Administrator
+    {
+        private static SqlCommand command;
+        public static bool LoginVerification(string username, string password)
+        {
+            string request = "SELECT COUNT(id_login) FROM T_Login WHERE username = @username AND password = @password";
+            command = new SqlCommand(request, DataBase.connection);
+            command.Parameters.Add(new SqlParameter("@username", username));
+            command.Parameters.Add(new SqlParameter("@password", password));
+            DataBase.connection.Open();
+            if((int)command.ExecuteScalar() > 0)
+            {
+                DataBase.connection.Close();
+                return true;
+            }
+            else
+            {
+                DataBase.connection.Close();
+                return false;
+            };
+        }
+    }
+}
